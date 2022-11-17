c319788e9a6532910d518bcc471652466a05c5aa
Ivaylo V Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2022-11-17 18:40:58 +0000

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_qd5ueoe", "ivo32", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    },"o7draHESyu11D_Tby")
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
